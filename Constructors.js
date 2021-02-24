ab9b57a0add3f0c150889e95d1db33dd3c98bcc5
Necip <ncptzgn@gmail.com>
2021-02-24 19:15:22 +0300

    OOP Tutorials

--- Constructors.js
+++ Constructors.js
@@ -15,24 +15,24 @@
 // console.log(emp1);
 
 
-function Employee(name,age,salary){  // Yapıcı fonksiyon - Constructor
-    this.name = name;
-    this.age = age;
-    this.salary=salary;
+// function Employee(name,age,salary){  // Yapıcı fonksiyon - Constructor
+//     this.name = name;
+//     this.age = age;
+//     this.salary=salary;
 
-    this.showInfos = function (){
-        console.log(this.name,this.age,this.salary);
-    }
+//     this.showInfos = function (){
+//         console.log(this.name,this.age,this.salary);
+//     }
 
 
 
-}
+// }
 
-const emp1 = new Employee("Mustafa",25,4000);
-const emp2 = new Employee("Ahmet",25,5000);
+// const emp1 = new Employee("Mustafa",25,4000);
+// const emp2 = new Employee("Ahmet",25,5000);
 
-emp1.showInfos();
-emp2.showInfos();
+// emp1.showInfos();
+// emp2.showInfos();
 
 
 // console.log(emp1);
