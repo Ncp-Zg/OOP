a7f8ecded9e6bd4c2b2333522917d73e0464f594
Necip <ncptzgn@gmail.com>
2021-02-24 17:52:36 +0300

    OOP Tutorials

---
+++ Constructors.js
@@ -0,0 +1,48 @@
+// console.log(this); // Global Scope
+
+// const emp1 = { // Object Literal
+//     name:"Mustafa",
+//     age:25,
+//     showInfos:function() {console.log("Bilgiler gösteriliyor");}
+// };
+// const emp2 = {
+//     name: "Ahmet",
+//     age:25
+// }
+// emp1.salary = 4000;
+// emp1.showInfos();
+
+// console.log(emp1);
+
+
+function Employee(name,age,salary){  // Yapıcı fonksiyon - Constructor
+    this.name = name;
+    this.age = age;
+    this.salary=salary;
+
+    this.showInfos = function (){
+        console.log(this.name,this.age,this.salary);
+    }
+
+
+
+}
+
+const emp1 = new Employee("Mustafa",25,4000);
+const emp2 = new Employee("Ahmet",25,5000);
+
+emp1.showInfos();
+emp2.showInfos();
+
+
+// console.log(emp1);
+
+
+// const emp1 = new Employee();
+// const emp2 = new Employee();
+// console.log(emp1);
+// console.log(emp2);
+
+
+// const date = new Date();
+// const date2 = new Date();
