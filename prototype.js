896958d791e165f67930ad8f25caa9af4a098197
Necip <ncptzgn@gmail.com>
2021-02-25 14:24:25 +0300

    OOP Tutorials

--- prototype.js
+++ prototype.js
@@ -3,17 +3,38 @@
 // object.name="Mustafa";
 // console.log(object);
 
+// function Employee(name,age){
+//     this.name = name;
+//     this.age = age;
+//     this.showInfos = function(){
+//         console.log("Bilgiler Gösteriliyor...");
+//     }
+//     this.toString = function(){
+//         console.log("Bu bir Employee objesi")
+//     }
+// }
+// const emp1 = new Employee("Mustafa",25);
+// console.log(emp1);
+
+// console.log(emp1.toString());
+
+// Part -2 
+
 function Employee(name,age){
     this.name = name;
     this.age = age;
-    this.showInfos = function(){
-        console.log("Bilgiler Gösteriliyor...");
-    }
-    this.toString = function(){
-        console.log("Bu bir Employee objesi")
-    }
+    // this.showInfos = function(){
+    //     console.log("isim: " + this.name + "Yaş" + this.age);
+    // }
+}
+
+Employee.prototype.showInfos = function(){
+        console.log("isim: " + this.name  + " Yaş: " + this.age);
 }
 const emp1 = new Employee("Mustafa",25);
-console.log(emp1);
+const emp2 = new Employee("Ahmet",23);
 
-console.log(emp1.toString());
+
+emp2.showInfos();
+console.log(emp1);
+console.log(emp2);
