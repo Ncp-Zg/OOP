c03ff52b0018d2ecd02763d7e018cbcda1d19d5a
Necip <ncptzgn@gmail.com>
2021-02-25 14:50:02 +0300

    OOP Tutorials

--- prototype.js
+++ prototype.js
@@ -20,21 +20,21 @@
 
 // Part -2 
 
-function Employee(name,age){
-    this.name = name;
-    this.age = age;
-    // this.showInfos = function(){
-    //     console.log("isim: " + this.name + "Yaş" + this.age);
-    // }
-}
+// function Employee(name,age){
+//     this.name = name;
+//     this.age = age;
+//     // this.showInfos = function(){
+//     //     console.log("isim: " + this.name + "Yaş" + this.age);
+//     // }
+// }
 
-Employee.prototype.showInfos = function(){
-        console.log("isim: " + this.name  + " Yaş: " + this.age);
-}
-const emp1 = new Employee("Mustafa",25);
-const emp2 = new Employee("Ahmet",23);
+// Employee.prototype.showInfos = function(){
+//         console.log("isim: " + this.name  + " Yaş: " + this.age);
+// }
+// const emp1 = new Employee("Mustafa",25);
+// const emp2 = new Employee("Ahmet",23);
 
 
-emp2.showInfos();
-console.log(emp1);
-console.log(emp2);
+// emp2.showInfos();
+// console.log(emp1);
+// console.log(emp2);
